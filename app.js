08115630dad915e5ada936458884571e8cccdc7c
taiwobenedict <ajayitaiwotunta@gmail.com>
2023-07-19 20:45:15 +0100

    updated

--- app.js
+++ app.js
@@ -26,4 +26,23 @@ const swiper = new Swiper(".swiper", {
   });
 
 
- 
+  const header = document.querySelector('.header')
+  let toggler = document.querySelector('.toggler');
+  
+  toggler.onclick = ()=> {
+    header.classList.add('navbar-bg')
+    toggler.firstChild.classList.toggle("fa-times")
+    toggler.firstChild.classList.toggle("fa-bars")
+  }
+  
+
+  document.body.onscroll = () => {
+  
+    if (document.body.scrollTop == 0) {
+      header.classList.remove('navbar-bg')
+    } else {
+      header.classList.add('navbar-bg')
+    }
+  
+  }
+  
